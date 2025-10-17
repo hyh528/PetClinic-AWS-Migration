f69dfebb069cba6dda39d86cb4b3bcfff02e7745
kskbr <sg977@naver.com>
2025-10-17 10:38:37 +0900

    ssm parameter store esc 연동

--- terraform/envs/dev/application/ecs.tf
+++ terraform/envs/dev/application/ecs.tf
@@ -1,26 +1,29 @@
-# 배포할 서비스 목록을 map으로 정의
+# 배포할 서비스별 목록 및 고유 설정, 파라미터 스토어 경로 이름 정의
+locals {
+  service_definitions = {
+    "customers-service" = { priority = 100, path_name = "customers/server" }
+    "vets-service"      = { priority = 110, path_name = "vets/server" }
+    "visits-service"    = { priority = 120, path_name = "visits/server" }
+    "admin-server"      = { priority = 130, path_name = "admin/server" }
+  }
+}
+
+# 2. for_each를 사용해 각 서비스의 포트 번호를 Parameter Store에서 가져옵니다.
+#    새로 추가한 path_name을 사용해 경로를 동적으로 구성합니다.
+data "aws_ssm_parameter" "service_ports" {
+  for_each = local.service_definitions
+  name     = "/petclinic/dev/${each.value.path_name}.port"
+}
+
+# 3. 위 정보들을 조합하여 ecs_services 맵을 동적으로 생성합니다.
 locals {
   ecs_services = {
-    "customers-service" = {
-      container_port = 8080
-      image_uri      = "${module.ecr.repository_urls["customers-service"]}:latest"
-      priority       = 100 # 리스너 규칙 우선순위 (겹치면 안 됨)
-    },
-    "vets-service" = {
-      container_port = 8080
-      image_uri      = "${module.ecr.repository_urls["vets-service"]}:latest"
-      priority       = 110
-    },
-    "visits-service" = {
-      container_port = 8080
-      image_uri      = "${module.ecr.repository_urls["visits-service"]}:latest"
-      priority       = 120
-    },
-    "admin-server" = {                                                       
-      container_port = 8080                                                  
-      image_uri      = "${module.ecr.repository_urls["admin-server"]}:latest"
-      priority       = 130                                                   
-     }                                                                        
+    for name, config in local.service_definitions : name => {
+      # 데이터 소스는 원래 서비스 이름(map의 key)으로 참조합니다.
+      container_port = tonumber(data.aws_ssm_parameter.service_ports[name].value)
+      image_uri      = "${module.ecr.repository_urls[name]}:latest"
+      priority       = config.priority
+    }
   }
 }
 
