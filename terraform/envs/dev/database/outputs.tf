f165e312c6784ad9ccc0cb8609c79f2d986191c6
pjj <kauze1998@gmail.com>
2025-10-17 20:44:35 +0900

    db_master_user_secret_arn -> outputs에 추가

--- terraform/envs/dev/database/outputs.tf
+++ terraform/envs/dev/database/outputs.tf
@@ -7,3 +7,9 @@ output "db_username_parameter_path" {
   description = "SSM 파라미터 스토어에 저장된 DB 사용자 이름의 경로"
   value       = aws_ssm_parameter.common["database.username"].name
 }
+
+output "db_master_user_secret_arn" {
+  description = "Aurora DB의 마스터 사용자 비밀번호가 저장된 Secrets Manager Secret의 ARN"
+  value       = aws_rds_cluster.petclinic_aurora_cluster.master_user_secret[0].secret_arn
+  sensitive   = true
+}
