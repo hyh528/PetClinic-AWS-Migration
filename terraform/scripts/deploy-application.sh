1f6a085eba243b641df4fc1dd0d07a677027a361
황영현 <corin528@gmail.com>
2025-10-19 10:02:23 +0900

    fix: 배포 스크립트 리전 하드코딩

--- terraform/scripts/deploy-application.sh
+++ terraform/scripts/deploy-application.sh
@@ -24,7 +24,7 @@ echo "이미지 태그: $IMAGE_TAG"
 
 # ECR 리포지토리 정보 가져오기
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text --profile petclinic-dev)
-REGION=$(aws configure get region --profile petclinic-dev)
+REGION="ap-southeast-2"
 
 if [ -z "$ACCOUNT_ID" ] || [ -z "$REGION" ]; then
     echo "❌ AWS 프로필 설정을 확인해주세요"
